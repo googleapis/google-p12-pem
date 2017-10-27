9e805535ebd043feea0388ec525b11833b54cc1e
Justin Beckwith <justin.beckwith@gmail.com>
2017-10-26 21:21:58 -0700

    Switch to TypeScript, break the API a bit

--- src/bin/gp12-pem.ts
+++ src/bin/gp12-pem.ts
@@ -1,15 +1,15 @@
 #!/usr/bin/env node
+import * as gp12 from '../index';
 
-var gp12 = require('../');
-var argv = process.argv;
-var p12Path = argv[2];
+const argv = process.argv;
+const p12Path = argv[2];
 
 if (!p12Path) {
   console.error('Please specify a *.p12 file to convert.');
   process.exit(1);
 }
 
-gp12(p12Path, function(err, pem) {
+gp12.getPem(p12Path, (err, pem) => {
   if (err) {
     console.log(err);
     process.exit(1);
---
+++ test/index.ts
@@ -0,0 +1,72 @@
+import * as assert from 'assert';
+import * as fs from 'fs';
+import {getPem} from '../src/index';
+
+describe('GoogleP12Pem', () => {
+  const GOODP12FILE = './test/assets/key.p12';
+  const BADP12FILE = './test/assets/badkey.p12';
+  const PEMFILENAME = './test/assets/key.pem';
+  const expectedPem = fs.readFileSync(PEMFILENAME, {encoding: 'utf8'});
+
+  it('should exist', () => {
+    assert.equal(typeof getPem, 'function');
+  });
+
+  it('should provide error on bad filename and callback', done => {
+    getPem('./badfilename.p12', (err: Error, pem: string) => {
+      assert(err);
+      assert.equal(pem, null);
+      assert.ok(err.message.startsWith('ENOENT'));
+      done();
+    });
+  });
+
+  it('should throw ENOENT on bad filename with no callback', done => {
+    getPem('./badfilename.p12')
+        .then(pem => {
+          assert.fail('Returned pem when it should have been rejected');
+        })
+        .catch(e => {
+          done();
+        });
+  });
+
+  it('should throw error on bad .p12 with no callback', done => {
+    getPem(BADP12FILE)
+        .then(pem => {
+          assert.fail('Returned pem when it should have been rejected');
+        })
+        .catch(e => {
+          done();
+        });
+  });
+
+  it('should return error on bad .p12 in callback', done => {
+    assert.doesNotThrow(() => {
+      getPem(BADP12FILE, (err: Error, pem: string) => {
+        assert.equal(null, null);
+        assert(err.message.indexOf('Too few bytes to read') > -1);
+        done();
+      });
+    });
+  });
+
+  it('should work async when provided a callback', done => {
+    getPem(GOODP12FILE, (err: Error, pem: string) => {
+      assert.ifError(err);
+      assert.equal(expectedPem, pem);
+      done();
+    });
+  });
+
+  it('should return a promise that resolves with the pem', () => {
+    getPem(GOODP12FILE)
+        .then(result => {
+          assert.equal(expectedPem, result);
+        })
+        .catch(e => {
+          assert.fail('Error getting pem from promise.');
+        });
+
+  });
+});
