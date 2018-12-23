2037f79445b6d8c5d1051ff2b286191686306752
Justin Beckwith <beckwith@google.com>
2018-12-22 22:38:29 -0800

    test: add system test, samples, and sample test

--- samples/quickstart.js
+++ samples/quickstart.js
@@ -5,4 +5,17 @@
  * See file LICENSE for detail or copy at https://opensource.org/licenses/MIT
  */
 
-console.warn(`no samples available 👻`);
+const {getPem} = require('google-p12-pem');
+
+/**
+ * Given a p12 file, convert it to the PEM format.
+ * @param {string} pathToCert The relative path to a p12 file.
+ */
+async function quickstart(pathToCert) {
+  const pem = await getPem(pathToCert);
+  console.log('The converted PEM:');
+  console.log(pem);
+}
+
+const args = process.argv.slice(2);
+quickstart(...args).catch(console.error);
