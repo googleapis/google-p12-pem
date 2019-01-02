1e9e9191ba9e05cfb16369ff00b1d17d98d4ad94
Justin Beckwith <justin.beckwith@gmail.com>
2019-01-02 11:20:15 -0600

    test: add system test, samples, and sample test (#131)

---
+++ samples/test/samples.test.js
@@ -0,0 +1,19 @@
+/**
+ * Copyright 2018 Google LLC
+ *
+ * Distributed under MIT license.
+ * See file LICENSE for detail or copy at https://opensource.org/licenses/MIT
+ */
+
+const execa = require('execa');
+const {assert} = require('chai');
+const path = require('path');
+
+const certPath = path.join(__dirname, '../assets/key.p12');
+
+describe('sample tests', () => {
+  it('should run the quickstart', async () => {
+    const {stdout} = await execa.shell(`node quickstart '${certPath}'`);
+    assert.match(stdout, /The converted PEM:/);
+  });
+});
--- system-test/system.ts
+++ system-test/system.ts
@@ -5,4 +5,22 @@
  * See file LICENSE for detail or copy at https://opensource.org/licenses/MIT
  */
 
-console.warn(`no system tests available 👻`);
+import * as assert from 'assert';
+import * as fs from 'fs';
+import * as path from 'path';
+import {promisify} from 'util';
+
+import {getPem} from '../src';
+
+const fixturePath = path.join(__dirname, '../../test/assets');
+const p12Path = path.join(fixturePath, 'key.p12');
+const pemPath = path.join(fixturePath, 'key.pem');
+const readFile = promisify(fs.readFile);
+
+describe('system', () => {
+  it('should convert a p12 into a pem', async () => {
+    const pem = await getPem(p12Path);
+    const expected = await readFile(pemPath, 'utf8');
+    assert.strictEqual(pem, expected);
+  });
+});
